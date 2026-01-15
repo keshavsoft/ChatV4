97c8119c4302338ce468ea07103cd51b0008d8cf
keshavsoft <nknnkumar@yahoo.com>
2026-01-15 09:30:35 +0530

    still not common

---
+++ app/src/main/java/com/example/compose/jetchat/feature/chatws/common/ChatRepository.kt
@@ -0,0 +1,4 @@
+package com.example.compose.jetchat.feature.chatws.common
+
+class ChatRepository {
+}
---
+++ app/src/main/java/com/example/compose/jetchat/feature/webSocketCode/ChatWebSocketManager.kt
@@ -0,0 +1,4 @@
+package com.example.compose.jetchat.feature.webSocketCode
+
+class ChatWebSocketManager {
+}
